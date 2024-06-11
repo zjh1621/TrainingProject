73175f8ed7ccd9028225aef19c97ba156fed0aa9
白月青竹 <1025685266@qq.com>
2024-06-11 20:29:43 +0800

    24.6.11练习

---
+++ src/main/java/com/example/mawen/MawenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mawen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MawenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MawenApplication.class, args);
+    }
+
+}
