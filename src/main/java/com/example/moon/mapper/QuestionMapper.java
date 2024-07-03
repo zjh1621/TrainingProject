4595e59879b08c7ffe6a18a7f614dded96bcebe9
moon-bamboo <1025685266@qq.com>
2024-07-03 20:28:44 +0800

    新增我的问题页面

--- src/main/java/com/example/moon/mapper/QuestionMapper.java
+++ src/main/java/com/example/moon/mapper/QuestionMapper.java
@@ -14,9 +14,15 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{tag})")
     void create(Question question);
 
+    @Select("select count(1) from question")
+    Integer count();
+
     @Select("select * from question limit #{offset},#{size}")
     List<Question> list(@Param(value="offset") Integer offset, @Param(value="size") Integer size);
 
-    @Select("select count(1) from question")
-    Integer count();
+    @Select("select * from question where creator = #{userId} limit #{offset},#{size}")
+    List<Question> listByUserId(@Param(value = "userId") Integer userId,@Param(value="offset") Integer offset, @Param(value="size") Integer size);
+
+    @Select("select count(1) from question where creator=#{userId}")
+    Integer countByUserId(@Param(value = "userId") Integer userId);
 }
--- src/main/java/com/example/moon/service/QuestionService.java
+++ src/main/java/com/example/moon/service/QuestionService.java
@@ -19,6 +19,48 @@ public class QuestionService {
     private QuestionMapper questionMapper;
     @Autowired
     private UserMapper userMapper;
+
+    public  PageDTO list(Integer userId, Integer page, Integer size) {
+        {
+            Integer totalCount = questionMapper.countByUserId(userId);//计算总条目数
+            Integer totalPage = 0;
+            //计算总页码
+            if (totalCount % size == 0) {
+                totalPage = totalCount / size;
+            } else {
+                totalPage = totalCount / size + 1;
+            }
+            //防止页码出错
+            if (page < 1) {
+                page = 1;
+            }
+            if (page > totalPage) {
+                page = totalPage;
+            }
+        }            //防止页码出错
+        //获取偏移量，（页码-1）乘每页数目，即是数据库查找字段的开始
+        Integer offset = size * (page - 1);
+        if(offset<0){
+            offset=0;
+        }
+        Integer totalCount = questionMapper.countByUserId(userId);//计算总条目数
+        List<Question> questionList = questionMapper.listByUserId(userId, offset, size);//获取数据库中的question对象的list
+        List<QuestionDTO> questionDTOList = new ArrayList<>();//传输层QuestionDTO对象的list
+
+        PageDTO pageDTO = new PageDTO();
+        for (Question question : questionList) {//遍历QuestionList
+            User user = userMapper.findById(question.getCreator());//查找对应的user
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);//将Question对象保存到DTO对象中
+            questionDTO.setUser(user);//为DTO元素加上id对应的User对象
+            questionDTOList.add(questionDTO);//将DTO元素加入队列
+        }
+        pageDTO.setQuestionDTOList(questionDTOList);//向pageDTO注入questionDTOList
+        pageDTO.setPagination(totalCount,page,size);//初始化pageDTO其他成员数据
+
+        return pageDTO;//返回DTO队列
+    }
+
     public PageDTO list(Integer page, Integer size) {
         {
             Integer totalCount = questionMapper.count();//计算总条目数
